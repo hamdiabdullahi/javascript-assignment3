7a55f2c2ab3861c1ffd6127d6e3e02dc1bcff346
student <abdullahihamdiabass@gmail.com>
2021-03-29 07:40:10 +0300

    first commit

---
+++ hamdi.js
@@ -0,0 +1,30 @@
+ const kiosk={
+     Fruit:"pawpaw",
+    get getFruit(){
+        return this.Fruit;
+    },
+    set setFruit(newFruit){
+        this.Fruit=newFruit
+    }
+         
+     };
+     console.log(kiosk.Fruit);
+     kiosk.newFruit="apple";
+     console.log(kiosk.newFruit);
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
